3a769d368b140214fb28cff71170860fe33a03e4
Jack Anderson <JackCA@users.noreply.github.com>
2022-08-19 11:14:08 -0700

    feat: export useModalState hook

--- packages/rainbowkit/src/index.ts
+++ packages/rainbowkit/src/index.ts
@@ -7,6 +7,7 @@ export {
   useAccountModal,
   useChainModal,
   useConnectModal,
+  useModalState,
 } from './components/RainbowKitProvider/ModalContext';
 export { useAddRecentTransaction } from './transactions/useAddRecentTransaction';
 export {
